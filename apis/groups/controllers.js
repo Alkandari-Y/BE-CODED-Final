bb3fee4cf01e615559eae3aab422db5a5ad1010d
fahadalduaij <fahad_osama@hotmail.com>
2021-12-14 22:14:24 +0300

    now anyone can update image and name of group

--- apis/groups/controllers.js
+++ apis/groups/controllers.js
@@ -5,180 +5,172 @@ const Message = require("../../db/models/Message");
 
 // Fetch one group
 exports.fetchGroupById = async (groupId, next) => {
-  try {
-    const group = await Group.findById(groupId);
-    return group;
-  } catch (error) {
-    next(error);
-  }
+	try {
+		const group = await Group.findById(groupId);
+		return group;
+	} catch (error) {
+		next(error);
+	}
 };
 
 // Fetch all groups the user(s) is in
 exports.fetchUserGroups = async (req, res, next) => {
-  try {
-    const groups = await Group.find().populate("polls").populate("chat");
-    //   .populate({
-    //   path: 'polls',
-    //   options: {
-    //     // limit: 2,
-    //     sort: { createdAt: -1 }
-    //   }
-    // })
-    //   .populate({
-    //     path: 'chat',
-    //     options: {
-    //       // limit: 2,
-    //       sort: { createdAt: -1 }
-    //     }
-    // });
-    //check for bug
-    // await groups
-    return res.json(groups);
-  } catch (error) {
-    next(error);
-  }
+	try {
+		const groups = await Group.find().populate("polls").populate("chat");
+		//   .populate({
+		//   path: 'polls',
+		//   options: {
+		//     // limit: 2,
+		//     sort: { createdAt: -1 }
+		//   }
+		// })
+		//   .populate({
+		//     path: 'chat',
+		//     options: {
+		//       // limit: 2,
+		//       sort: { createdAt: -1 }
+		//     }
+		// });
+		//check for bug
+		// await groups
+		return res.json(groups);
+	} catch (error) {
+		next(error);
+	}
 };
 
 // Create group
 exports.createGroup = async (req, res, next) => {
-  try {
-    if (req.file) {
-      req.body.image = `/${req.file.path}`;
-      req.body.image = req.body.image.replace("\\", "/");
-    }
-    req.body.owner = req.user._id;
-    req.body.members = [req.user._id];
+	try {
+		if (req.file) {
+			req.body.image = `/${req.file.path}`;
+			req.body.image = req.body.image.replace("\\", "/");
+		}
+		req.body.owner = req.user._id;
+		req.body.members = [req.user._id];
 
-    const newGroup = await Group.create(req.body);
+		const newGroup = await Group.create(req.body);
 
-    res.status(201).json(newGroup);
-  } catch (error) {
-    next(error);
-  }
+		res.status(201).json(newGroup);
+	} catch (error) {
+		next(error);
+	}
 };
 
 // Update Group
 exports.updateGroup = async (req, res, next) => {
-  try {
-    if (req.group.owner.toString() === req.user._id.toString()) {
-      if (req.file) {
-        req.body.image = `/media/${req.file.filename}`;
-        req.body.image = req.body.image.replace("\\", "/");
-      }
+	try {
+		if (req.file) {
+			req.body.image = `/media/${req.file.filename}`;
+			req.body.image = req.body.image.replace("\\", "/");
+		}
 
-      const foundGroup = await Group.findByIdAndUpdate(
-        req.group._id,
-        req.body,
-        {
-          new: true,
-          runValidators: true,
-        }
-      );
+		const foundGroup = await Group.findByIdAndUpdate(req.group._id, req.body, {
+			new: true,
+			runValidators: true,
+		});
 
-      res.status(201).json(foundGroup);
-    } else {
-      next({ status: 401, message: "You are not the Owner" });
-    }
-  } catch (error) {
-    next(error);
-  }
+		res.status(201).json(foundGroup);
+	} catch (error) {
+		next(error);
+	}
 };
 
 // Delete Group
 exports.deleteGroup = async (req, res, next) => {
-  try {
-    if (req.group.owner.toString() === req.user._id.toString()) {
-      await req.group.remove();
-      res.status(204).end();
-    } else {
-      next({ status: 401, message: "You are not the Owner" });
-    }
-  } catch (error) {
-    next(error);
-  }
+	try {
+		if (req.group.owner.toString() === req.user._id.toString()) {
+			await req.group.remove();
+			res.status(204).end();
+		} else {
+			next({ status: 401, message: "You are not the Owner" });
+		}
+	} catch (error) {
+		next(error);
+	}
 };
 
 // Add users to group as members
 exports.addMembersToGroup = async (req, res, next) => {
-  try {
-    if (!req.user._id.equals(req.group.owner._id)) {
-      return next({
-        status: 401,
-      });
-    }
+	try {
+		if (!req.user._id.equals(req.group.owner._id)) {
+			return next({
+				status: 401,
+			});
+		}
 
-    const newMember = await User.findOne({ phoneNumber: req.body.phoneNumber });
+		const newMember = await User.findOne({ phoneNumber: req.body.phoneNumber });
 
-    const findNumber = req.group.members.includes(newMember._id);
+		const findNumber = req.group.members.includes(newMember._id);
 
-    if (!findNumber) {
-      const updatedGroup = await Group.findByIdAndUpdate(
-        req.params.groupId,
-        {
-          $push: { members: newMember._id },
-        },
-        { new: true, runValidators: true }
-      );
-      return res.json(updatedGroup);
-    } else {
-      next({ status: 500, message: "Member already exits in the group" });
-    }
-  } catch (error) {
-    next(error);
-  }
+		if (!findNumber) {
+			const updatedGroup = await Group.findByIdAndUpdate(
+				req.params.groupId,
+				{
+					$push: { members: newMember._id },
+				},
+				{ new: true, runValidators: true }
+			);
+			return res.json(updatedGroup);
+		} else {
+			next({ status: 500, message: "Member already exits in the group" });
+		}
+	} catch (error) {
+		next(error);
+	}
 };
 
 exports.addMoviePoll = async (req, res, next) => {
-  try {
-    req.body.owner = req.user._id;
-    req.body.group = req.group._id;
-    const newPoll = await Poll.create(req.body);
+	try {
+		req.body.owner = req.user._id;
+		req.body.group = req.group._id;
+		const newPoll = await Poll.create(req.body);
 
-    await Group.findByIdAndUpdate(req.group._id, {
-      $push: { polls: newPoll._id },
-    });
+		await Group.findByIdAndUpdate(req.group._id, {
+			$push: { polls: newPoll._id },
+		});
 
-    res.status(201).json(newPoll);
-  } catch (error) {
-    next(error);
-  }
+		res.status(201).json(newPoll);
+	} catch (error) {
+		next(error);
+	}
 };
 
 exports.addChat = async (req, res, next) => {
-  try {
-    const newMessage = await Message.create(req.body);
-    const updatedGroup = await Group.findByIdAndUpdate(
-      req.group._id,
-      { $push: { chat: newMessage._id } },
-      { new: true, runValidators: true }
-    ).populate("chat");
-    res.status(201).json(newMessage);
-  } catch (error) {
-    next(error);
-  }
+	try {
+		const newMessage = await Message.create(req.body);
+		const updatedGroup = await Group.findByIdAndUpdate(
+			req.group._id,
+			{ $push: { chat: newMessage._id } },
+			{ new: true, runValidators: true }
+		).populate("chat");
+		res.status(201).json(newMessage);
+	} catch (error) {
+		next(error);
+	}
 };
 
 exports.leaveGroup = async (req, res, next) => {
-  try {
-    const updatedMemberList = req.group.members.find(
-      (member) => member === req.user._id
-    );
+	try {
+		const updatedMemberList = req.group.members.find(
+			(member) => member === req.user._id
+		);
 
-    if (!updatedMemberList) {
-      next({
-        status: 404,
-        message: "User was not found!",
-      });
-    }
+		if (!updatedMemberList) {
+			next({
+				status: 404,
+				message: "User was not found!",
+			});
+		}
 
-    const updatedGroup = await Group.findByIdAndUpdate(
-      req.group._id,
-      { $pull: { members: req.user._id } },
-      { new: true, runValidators: true }
-    );
+		const updatedGroup = await Group.findByIdAndUpdate(
+			req.group._id,
+			{ $pull: { members: req.user._id } },
+			{ new: true, runValidators: true }
+		);
 
-    res.status(200).json(updatedGroup);
-  } catch {
-    console.log("");
-  }
+		res.status(200).json(updatedGroup);
+	} catch {
+		console.log("");
+	}
 };
